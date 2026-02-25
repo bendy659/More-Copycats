659c37daa914bf871b95489eab412cb2bf68e50a
_BENDY659_ <109421782+bendy659@users.noreply.github.com>
2026-02-25 13:12:33 +0900

    Delete backup directory

--- backup/src_backup_20260224_133751/src/main/java/ru/benos_codex/more_copycats/CopycatBlockEntityTypeHelper.java
+++
@@ -1,7 +0,0 @@
-package ru.benos_codex.more_copycats;
-
-import net.minecraft.world.level.block.entity.BlockEntityType;
-
-public class CopycatBlockEntityTypeHelper {
-    public static final ThreadLocal<BlockEntityType<?>> OVERRIDE = new ThreadLocal<>();
-}
--- backup/src_backup_20260224_133751/src/main/kotlin/ru/benos_codex/more_copycats/item/CopycatUvToolItem.kt
+++
@@ -1,7 +0,0 @@
-package ru.benos_codex.more_copycats.item
-
-import net.minecraft.world.item.Item.Properties
-
-import net.minecraft.world.item.Item
-
-class CopycatUvToolItem(props: Properties) : Item(props)
